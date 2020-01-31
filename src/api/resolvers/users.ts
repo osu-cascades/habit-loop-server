6bbaf365e82176ec72e8d07665ab3438e31c11d0
Johnny Phelan <phelanjo@oregonstate.edu>
2020-01-31 11:02:15 -0800

    change query to get by email

--- src/api/resolvers/users.ts
+++ src/api/resolvers/users.ts
@@ -47,7 +47,7 @@ const resolvers: IResolvers = {
 
     async getUserRole(instance, args, { user, UserModel, logger }) {
       try {
-        const result = await UserModel.getByIdOnly(user.user_id);
+        const result = await UserModel.getByEmail(user.email);
         return _.get(result, 'Items[0]');
       } catch (err) {
         logger.error(`Problem getting user: ${err}`);
