7f61f4e1bd2084784845f8e48fcedd0dabc45f80
Johnny Phelan <phelanjo@oregonstate.edu>
2020-03-04 14:46:37 -0800

    Remove getAllUserGroups query

--- src/api/schema/user.ts
+++ src/api/schema/user.ts
@@ -8,7 +8,6 @@ export default gql`
     getGroupLeaderboard(item_id: String!): [Streak]
     getUserGroups: [Group]
     getAllGroups: [Group]
-    getAllGroupUsers(item_id: String): [User]
   }
 
   extend type Mutation {
